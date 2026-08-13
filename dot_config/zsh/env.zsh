2e1dfe2f22cf90907a342275d207d8dbca944ace
onodai145 <onodai145@noreply.git.omhnc.net>
2026-08-13 10:00:50 +0900

    いろいろふえた

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -2,4 +2,5 @@ export EDITOR=vim
 export LANG=ja_JP.UTF-8
 export KCODE=u
 export AUTOFEATURE=true
+export SSH_AUTH_SOCK=/home/`whoami`/.bitwarden-ssh-agent.sock
 
--- dot_config/zsh/history.zsh
+++ dot_config/zsh/history.zsh
@@ -8,11 +8,11 @@ setopt share_history
 setopt hist_reduce_blanks
 
 # マッチしたコマンドのヒストリを表示できるようにする
-autoload history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^P" history-beginning-search-backward-end
-bindkey "^N" history-beginning-search-forward-end
+#autoload history-search-end
+#zle -N history-beginning-search-backward-end history-search-end
+#zle -N history-beginning-search-forward-end history-search-end
+#bindkey "^P" history-beginning-search-backward-end
+#bindkey "^N" history-beginning-search-forward-end
 
 # すべてのヒストリを表示する
 function history-all { history -E 1 }
